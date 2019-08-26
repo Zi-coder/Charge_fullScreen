965aa78e851d5d8fc3a4337374e2bb0d496a783f
Zi-coder <akofficial238444@gmail.com>
2019-08-26 23:03:53 +0530

    Final update

--- app/src/main/java/com/anil/chargestatus/MainActivity.java
+++ app/src/main/java/com/anil/chargestatus/MainActivity.java
@@ -1,16 +1,16 @@
 package com.anil.chargestatus;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    public void hide(View view){
+        View mDecorView = getWindow().getDecorView();
+        mDecorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION  | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
     private BatteryReceiver mBatteryReceiver = new BatteryReceiver();
     private IntentFilter mIntentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
     @Override
@@ -18,23 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         registerReceiver(mBatteryReceiver,mIntentFilter);
     }
-
     @Override
     protected void onPause() {
         unregisterReceiver(mBatteryReceiver);
         super.onPause();
-
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         View mDecorView = getWindow().getDecorView();
         mDecorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION  | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE);
-
-
-
-
-    }
+     }
 }
